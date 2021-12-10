40a4845d84f7d92c97e21af02e6ec3c0bd1d6f6b
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2021-12-10 00:10:04 -0500

    Avoid using dict items directly
    Dict items cannot be pickled.

--- wrapdisc/wrapdisc.py
+++ wrapdisc/wrapdisc.py
@@ -18,14 +18,14 @@ class Vars:
         variable_lengths = [len(v) for v in self._variables]
         self._variables_slices = {
             var: slice(tot_len - cur_len, tot_len) for var, cur_len, tot_len in zip(self._variables, variable_lengths, itertools.accumulate(variable_lengths))
-        }.items()
+        }
         self.decoded_len = len(self._variables)
         self.encoded_len = sum(variable_lengths)
 
     def __getitem__(self, encoded: EncodingType) -> tuple:
         """Return the decoded solution from its encoded solution."""
         assert len(encoded) == self.encoded_len
-        decoded = tuple(var[encoded[var_slice]] for var, var_slice in self._variables_slices)
+        decoded = tuple(var[encoded[var_slice]] for var, var_slice in self._variables_slices.items())
         assert len(decoded) == self.decoded_len
         return decoded
 
